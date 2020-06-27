90aeeabd5f031eb23c0f40b4bb8e062e9732e9a1
Chanwoong Kim <ykdy3951@naver.com>
2020-06-28 03:17:11 +0900

    complete base

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
-function App() {
-  return;
-  <div className='App' />;
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading: false }); //축약
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? 'Loading...'
+          : movies.map((movie) => {
+              console.log(movie);
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
+      </div>
+    );
+  }
 }
 
 export default App;
