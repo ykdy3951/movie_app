355accc5ef4a49034ec85fc5421dbf3d237a59f6
Chanwoong Kim <ykdy3951@naver.com>
2020-06-28 23:42:13 +0900

    Updates

---
+++ precache-manifest.bee59ab1dd8182df7cef5c238b62ee22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae633ba4f357058446eadb4c8d4a55a",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "0d7460c0dfc3fba17ae8",
+    "url": "/movie_app/static/css/main.8b9bb7c5.chunk.css"
+  },
+  {
+    "revision": "e6ee8035cbef69ff6a6d",
+    "url": "/movie_app/static/js/2.ec260248.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app/static/js/2.ec260248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d7460c0dfc3fba17ae8",
+    "url": "/movie_app/static/js/main.4042e95d.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
