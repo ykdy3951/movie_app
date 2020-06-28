d1062db796ed9d287b89bed50f2a206389a905b3
Chanwoong Kim <ykdy3951@naver.com>
2020-06-29 00:19:38 +0900

    complete

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -14,25 +14,27 @@ class Detail extends React.Component {
     if (location.state) {
       return (
         <section className='container'>
-          <div className='movie'>
-            <img
-              src={location.state.poster}
-              alt={location.state.title}
-              title={location.state.title}
-            />
-            <div className='movie__data'>
-              <h3 className='movie__title'>{location.state.title}</h3>
-              <h5 className='movie__year'>{location.state.year}</h5>
-              <ul className='movie__genres'>
-                {location.state.genres.map((genre, index) => {
-                  return (
-                    <li key={index} className='movie__genre'>
-                      {genre}
-                    </li>
-                  );
-                })}
-              </ul>
-              <p className='movie__summary'>{location.state.summary}</p>
+          <div className='movies'>
+            <div className='movie'>
+              <img
+                src={location.state.poster}
+                alt={location.state.title}
+                title={location.state.title}
+              />
+              <div className='movie__data'>
+                <h2 className='movie__title'>{location.state.title}</h2>
+                <h4 className='movie__year'>{location.state.year}</h4>
+                <ul className='movie__genres'>
+                  {location.state.genres.map((genre, index) => {
+                    return (
+                      <li key={index} className='movie__genre'>
+                        {genre}
+                      </li>
+                    );
+                  })}
+                </ul>
+                <p className='movie__summary'>{location.state.summary}</p>
+              </div>
             </div>
           </div>
         </section>
